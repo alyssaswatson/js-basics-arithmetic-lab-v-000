6dc4af60b416a88b727eedf36a653cd55a1ce8d6
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-06 01:54:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ var newID = oldID + 1000000000;
 
 var ageIsValid = Number.isInteger(currentAge);
 
-var randomNumber = (Math.random() * 10);
+var randomNumber = Math.random();
 
 var randomInteger = Math.floor(Math.random() * 10) + 1;
